ec7b902e67b28d16a520b5dbbb1b74543f9ab810
Vaibhav R. Kumar <vaibhavk667@gmail.com>
2020-03-21 18:00:20 -0700

    Added schema

---
+++ schema.sql
@@ -0,0 +1,43 @@
+
+CREATE TABLE users(
+    id INTEGER AUTO_INCREMENT,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password_string VARCHAR(255) UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE posts(
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    user_id INTEGER NOT NULL,
+    username INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (username) REFERENCES users (username)
+);
+
+CREATE TABLE avatars(
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    image_url VARCHAR(255) NOT NULL,
+    user_id INTEGER NOT NULL,
+    username INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (username) REFERENCES users (username)
+);
+
+CREATE TABLE comments(
+    id INTEGER NOT NULL AUTO_INCREMENT,
+	comment_string VARCHAR(255) NOT NULL,
+	user_id INTEGER NOT NULL,
+    username INTEGER NOT NULL,
+	avatar_id INTEGER NOT NULL,
+	created_at TIMESTAMP DEFAULT NOW(),
+	PRIMARY KEY (id),
+	FOREIGN KEY (user_id) REFERENCES users(id),
+	FOREIGN KEY (avatar_id) REFERENCES avatars(id),
+    FOREIGN KEY (username) REFERENCES users (username)
+);
