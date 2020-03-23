bd3dbd6b1794e0ac67efbc7e6bab4833c25d57b1
Vaibhav R. Kumar <vaibhavk667@gmail.com>
2020-03-23 12:20:27 -0700

    Create user complete

--- config/insert.php
+++ config/insert.php
@@ -1,16 +1,26 @@
 <!-- this file controls Create User via email form -->
 <?php
-    require('email.php');
+    //require('email.php');
     require('db_connect.php');    
     
-    //prepared statement 
-    $email = $_GET['emailid'];
-    $username = $_GET['username'];
-    $password = $_GET['password'];
+    if(isset($_POST['submitEmail'])){
+        //prepared statement 
+        $email = $_POST['emailid'];
+        $username = $_POST['username'];
+        $password = $_POST['password'];
 
-    //construct the query
-    //$sql = "insert into users(username,email,password_string) values('$username', '$email', '$password')";
+        //construct the query
+        $sql = "insert into users(username,email,password_string) values(?,?,?)";
 
-    //close cnx
-    mysqli_close($conn);
+        $stmt = $conn->prepare($sql);
+        //bind variable
+        $stmt->bind_param("sss", $username, $email, $password);
+        //execute the prepared statement
+        $stmt->execute();
+        echo "user added successfully";
+        //close the statement
+        $stmt->close();
+        //close cnx
+        mysqli_close($conn);
+    }
 ?>
