e46ae917e0fc86a96e5b8a258245324e4363f45f
Vaibhav R. Kumar <vaibhavk667@gmail.com>
2020-03-23 22:10:49 -0700

    Fixed Footer

--- email.php
+++ email.php
@@ -3,34 +3,36 @@
 
 <!-- body -->
   <body>
-    <a name="SignUp"></a>
-    <h1>Fill up Your Details Below:</h1>
-    <p>Create an account to make Blogs, to share Blogs and to comment on Blogs</p>
-    <br>
+    <section class="emailForm">
+      <a name="SignUp"></a>
+      <h1>Fill up Your Details Below:</h1>
+      <p>Create an account to make Blogs, to share Blogs and to comment on Blogs</p>
+      <br>
 
-    <!-- email form -->
-    <?php require_once('config/processRegister.php'); ?>
-    <form action="config/processRegister.php" name="emailform" method="POST">
-      <label>Email</label><br>
-      <input type="text" id="emailid" name="emailid" placeholder="email id" required><br>
-      <label>Username</label><br>
-      <input type="text" id="username" name="username" placeholder="username" required><br>
-      <label>Password </label><br>
-      <input type="text" id="password" name="password" placeholder="password" required><br>
-      <label>Repeat Password </label><br>
-      <input type="text" id="password" name="repeatPassword" placeholder="password" required><br>
+      <!-- email form -->
+      <?php require_once('config/processRegister.php'); ?>
+      <form action="config/processRegister.php" name="emailform" method="POST">
+        <label>Email</label><br>
+        <input type="text" id="emailid" name="emailid" placeholder="email id" required><br>
+        <label>Username</label><br>
+        <input type="text" id="username" name="username" placeholder="username" required><br>
+        <label>Password </label><br>
+        <input type="text" id="password" name="password" placeholder="password" required><br>
+        <label>Repeat Password </label><br>
+        <input type="text" id="password" name="repeatPassword" placeholder="password" required><br>
 
-      <button class="button" type="submit" name="submitEmail" onclick="Javascript:emailValidate();">
-        Create Account
-      </button>
-    </form>
+        <button class="button" type="submit" name="submitEmail" onclick="Javascript:emailValidate();">
+          Create Account
+        </button>
+      </form>
 
 
-    <p>Already have a account? <a href="signIn.php">Sign in</a></p>
-    <p>To make Blogged work, we log user data and share it with service providers. Click “Sign Up” above to accept Blogged's
-    <a href="https://policy.medium.com/medium-terms-of-service-9db0094a1e0f">Terms of Service</a> &
-    <a href="https://policy.medium.com/medium-privacy-policy-f03bf92035c9">Privacy Policy</a>.
-    </p>
+      <p>Already have a account? <a href="signIn.php">Sign in</a></p>
+      <p>To make Blogged work, we log user data and share it with service providers. Click “Sign Up” above to accept Blogged's
+      <a href="https://policy.medium.com/medium-terms-of-service-9db0094a1e0f">Terms of Service</a> &
+      <a href="https://policy.medium.com/medium-privacy-policy-f03bf92035c9">Privacy Policy</a>.
+      </p>
+    </section>
   </body>
   <!-- footer -->
   <?php include('footer.php');?>
--- login.php
+++ login.php
@@ -2,15 +2,17 @@
 <?php include('header.php');?>
 
 <body>
-    <h2>Login with Email</h2>
-    <?php require_once('config/processLogin.php'); ?>
-    <form action="config/processLogin.php" name="loginform" method="POST">
-        <label>Email</label>
-        <input type="email" id="loginemail" name="loginemail" placeholder="Enter email id" required><br>
-        <label>Password</label>
-        <input type="password" id="loginpass" name="loginpass" placeholder="Enter password" required><br>
-        <button type="submit" name="submitEmail">Log In</button>
-    </form>
+    <section class="emailLogin">
+        <h2>Login with Email</h2>
+        <?php require_once('config/processLogin.php'); ?>
+        <form action="config/processLogin.php" name="loginform" method="POST">
+            <label>Email</label>
+            <input type="email" id="loginemail" name="loginemail" placeholder="Enter email id" required><br>
+            <label>Password</label>
+            <input type="password" id="loginpass" name="loginpass" placeholder="Enter password" required><br>
+            <button type="submit" name="submitEmail">Log In</button>
+        </form>
+    </section>
 </body>
  <!-- footer -->
  <?php include('footer.php');?>
