e5fe09eaecd22878d2d9b8e8d6319dc901718469
Vaibhav R. Kumar <vaibhavk667@gmail.com>
2020-03-27 13:49:36 -0700

    working on dashbaord

--- userView/dashboard.php
+++ userView/dashboard.php
@@ -10,7 +10,29 @@
         //show all blog posts
         $sql = "select * from posts";
         $result = $conn->query($sql) or die($conn->error);
-        $row = $result->fetch_assoc();
+
+        
+        if(mysqli_num_rows($result)>0){
+            while($row = $result->fetch_assoc()){
+                $title = $row['title_str'];
+                $body = $row['body_str'];
+?>
+        <!-- html tag for displaying posts -->
+        <div class="row">
+            <div class="col-lg-5">
+                <strong> <?php echo $title; ?> </strong>                
+                <p> <?php echo $body; ?> </p>   
+                <button>Edit</button>  
+                <button>View More</button>       
+            </div>
+        </div>
+
+<?php
+                // echo'
+                
+                // ';
+            }
+        }
         
         //footer
         include('../partials/footer.php');
