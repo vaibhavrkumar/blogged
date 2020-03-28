cdc2c89fa84ad0403c3a32a510c48dbfd721afef
Vaibhav R. Kumar <vaibhavk667@gmail.com>
2020-03-28 13:45:31 -0700

    viewBlog.php completed

--- userView/dashboard.php
+++ userView/dashboard.php
@@ -16,24 +16,24 @@
             while($row = $result->fetch_assoc()){
                 $title = $row['title_str'];
                 $body = $row['body_str'];
+                $post_id = $row['id'];
 ?>
-        <!-- html tag for displaying posts -->
-        <div class="row">
-            <div class="col-lg-5">
-                <strong> <?php echo $title; ?> </strong>                
-                <p> <?php echo $body; ?> </p>   
-                <button>Edit</button>  
-                <button>View More</button>       
-            </div>
+    <!-- html tag for displaying posts -->
+    <div class="row">            
+        <div class="col-lg-5">
+            <!-- Displaying each posts -->
+            <strong> <?php echo $title; ?> </strong>                
+            <p> <?php echo $body; ?> </p>   
+            <!-- Edit and Delete  -->
+            <button><a href=../userView/viewBlog.php?id=<?php echo $post_id;?>>View</a></button>
+            <button><a href=../userView/editBlog.php?id=<?php echo $post_id;?>>Edit</a></button>
+            <button><a href=../userView/deleteBlog.php?id=<?php echo $post_id;?>>Delete</a></button>
         </div>
+    </div>
 
-<?php
-                // echo'
-                
-                // ';
+<?php               
             }
-        }
-        
+        }        
         //footer
         include('../partials/footer.php');
     }
